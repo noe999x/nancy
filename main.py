3e3ff908a6ba66ad9c371046d944a4deb23a4831
ikz <68614343+noe999x@users.noreply.github.com>
2022-12-03 22:34:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ except:pass
 try:
     os.system('clear')
     print('Tools sedang dalam pemeliharan');time.sleep(4)
-    print('untuk info selanjutnya bisa hubungi saya!');time.sleep(3)
+    print('Untuk info selanjutnya bisa hubungi saya!');time.sleep(3)
 except:pass
 def atmin():
     print('\nKetik oke untuk menuju profile facebook author')
-    nancy = input('\nKlik enter untuk exit program\n\n')
+    nancy = input('\nTekan enter untuk exit program\n\n')
     if nancy in ['oke','ok']:
-        print('Anda akan diarahkan ke profile facebook author')
+        print('Anda akan diarahkan ke halaman profile facebook author')
         time.sleep(2)
         os.system('xdg-open https://www.facebook.com/bagasekaapr')
     elif nancy in['',' ']:
-        print('\nterimakasih sudah mau menunggu!\n maaf atas ketidaknyamanannya ya!');exit()
+        print('\nTerimakasih sudah mau menunggu~\n Maaf atas ketidaknyamanannya ya!');exit()
     else:exit()
 atmin()
